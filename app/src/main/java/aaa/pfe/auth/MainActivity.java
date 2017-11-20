bbb2a3b7bbe431739792690553d92a5b931372f9
Abdelkarim Andolerzak <Karim@MacBook-Pro-de-Abdelkarim.local>
2017-11-20 11:56:13 +0100

    Edit drawer interaction

--- app/src/main/java/aaa/pfe/auth/MainActivity.java
+++ app/src/main/java/aaa/pfe/auth/MainActivity.java
@@ -1,5 +1,6 @@
 package aaa.pfe.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,11 +81,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_pin) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_pattern) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_passface) {
 
         }
 
---
+++ app/src/main/java/aaa/pfe/auth/view/PassFace.java
@@ -0,0 +1,15 @@
+package aaa.pfe.auth.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import aaa.pfe.auth.R;
+
+public class PassFace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_face);
+    }
+}
