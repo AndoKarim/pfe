f56a8b2534b258029ed9ef33e0eec7959718e423
Andokarim <ando.karim@gmail.com>
2017-11-20 12:56:51 +0100

    PassFaceAdmin send settings, back to PassFace page.

--- app/src/main/java/aaa/pfe/auth/view/passface/PassFaceAdminActivity.java
+++ app/src/main/java/aaa/pfe/auth/view/passface/PassFaceAdminActivity.java
@@ -1,7 +1,11 @@
 package aaa.pfe.auth.view.passface;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import aaa.pfe.auth.R;
 
@@ -12,13 +16,24 @@ public class PassFaceAdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_face_admin);
 
-        getSupportActionBar().setTitle("Admin Passface");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.save);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Saved, TODO, Send to server", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                    }
+                }, 1000);
+            }
+        });
+
+
+
+
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        finish();
-        return true;
-    }
 }
