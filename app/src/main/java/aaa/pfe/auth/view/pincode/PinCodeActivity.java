6be54c5762a78dacebf9cb68478341ef3107622e
Antoine Rabenandrasana <antoine.rabenandrasana@etu.unice.fr>
2017-11-21 13:54:28 +0100

    Pincode first view

--- app/src/main/java/aaa/pfe/auth/view/pincode/PinCodeActivity.java
+++ app/src/main/java/aaa/pfe/auth/view/pincode/PinCodeActivity.java
@@ -1,23 +1,65 @@
 package aaa.pfe.auth.view.pincode;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import aaa.pfe.auth.R;
+import aaa.pfe.auth.view.pincodeview.IndicatorDots;
+import aaa.pfe.auth.view.pincodeview.PinLockListener;
+import aaa.pfe.auth.view.pincodeview.PinLockView;
 
 public class PinCodeActivity extends AppCompatActivity {
+    public static final String TAG = "PinLockView";
+
+    private PinLockView mPinLockView;
+    private IndicatorDots mIndicatorDots;
+
+    private PinLockListener mPinLockListener = new PinLockListener() {
+        @Override
+        public void onComplete(String pin) {
+            Log.d(TAG, "Pin complete: " + pin);
+        }
+
+        @Override
+        public void onEmpty() {
+            Log.d(TAG, "Pin empty");
+        }
+
+        @Override
+        public void onPinChange(int pinLength, String intermediatePin) {
+            Log.d(TAG, "Pin changed, new length " + pinLength + " with intermediate pin " + intermediatePin);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pass_face);
+        setContentView(R.layout.activity_pin_code);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Pincode");
+
+
+        //PINLOCKVIEW
+        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
+        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
+
+        mPinLockView.attachIndicatorDots(mIndicatorDots);
+        mPinLockView.setPinLockListener(mPinLockListener);
+
+        //mPinLockView.setCustomKeySet(new int[]{2, 3, 1, 5, 9, 6, 7, 0, 8, 4});
+        //mPinLockView.enableLayoutShuffling();
+
+        mPinLockView.setPinLength(4);
+        mPinLockView.setTextColor(ContextCompat.getColor(this, R.color.greyish));
+
+        mIndicatorDots.setIndicatorType(IndicatorDots.IndicatorType.FILL_WITH_ANIMATION);
     }
 
     @Override
