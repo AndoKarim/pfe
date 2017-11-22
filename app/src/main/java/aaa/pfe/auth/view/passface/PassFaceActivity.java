84cb92e60ea827fb2d06cd94d2d53db49948dd55
Andokarim <ando.karim@gmail.com>
2017-11-22 11:03:31 +0100

    PassFaceActivity showing n images and display them. When you click on one, it shows its name.

--- app/src/main/java/aaa/pfe/auth/view/passface/PassFaceActivity.java
+++ app/src/main/java/aaa/pfe/auth/view/passface/PassFaceActivity.java
@@ -5,11 +5,21 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import aaa.pfe.auth.R;
 
 public class PassFaceActivity extends AppCompatActivity {
+    static final String[] ANIMALS = new String[]{
+            "Lion", "Tigre", "Ours", "Abeille", "Chien", "Chat", "Mouton", "Baleine", "Cheval"};
+    static final String[] NUMBERS = new String[]{
+            "0", "1", "2", "3", "4", "5", "6", "7", "8"};
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +28,28 @@ public class PassFaceActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Passface");
+
+
+        gridView = (GridView) findViewById(R.id.gridView);
+
+        gridView.setAdapter(new ImageAdapter(this, NUMBERS));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //String picName =
+                ImageView i = (ImageView) ((LinearLayout) v).getChildAt(0);
+                String picName = (String) i.getTag();
+                Toast.makeText(
+                        getApplicationContext(), picName, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
     }
 
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
@@ -40,7 +68,7 @@ public class PassFaceActivity extends AppCompatActivity {
 
         if (id == R.id.pullFromServer) {
             // do something here
-            Toast t =Toast.makeText(this,"Pull From Server",Toast.LENGTH_SHORT);
+            Toast t = Toast.makeText(this, "Pull From Server", Toast.LENGTH_SHORT);
             t.show();
         }
 
